53e895ce91428db44e9ad38859fbdd765fc6f498
Alex <mattermoran@gmail.com>
2018-08-06 16:03:06 +0800

    fix dev

--- src/Components/Remote.js
+++ src/Components/Remote.js
@@ -1,5 +1,5 @@
-const dev = true;
+const dev = false;
 
 export const API = dev ? 'http://localhost:1234' : 'https://dev.scry.info/scry2';
 
-export const Publisher = !dev ? 'http://localhost:2222' : 'https://dev.scry.info:443/meta';
+export const Publisher = dev ? 'http://localhost:2222' : 'https://dev.scry.info:443/meta';
